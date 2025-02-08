33e54a7514909a73aa02520cedba7d2c4fdbb032
bartek <bartoszluka1@gmail.com>
2025-02-08 22:52:29 +0100

    Remove mini.statusline

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -752,38 +752,6 @@ return {
         end,
     },
     {
-        "mini.statusline",
-        event = "DeferredUIEnter",
-        after = function()
-            require("mini.statusline").setup({
-                content = {
-                    active = function()
-                        local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
-                        local git = MiniStatusline.section_git({ trunc_width = 40 })
-                        local diff = MiniStatusline.section_diff({ trunc_width = 75 })
-                        local diagnostics = MiniStatusline.section_diagnostics({ trunc_width = 75 })
-                        local lsp = MiniStatusline.section_lsp({ trunc_width = 75 })
-                        local filename = MiniStatusline.section_filename({ trunc_width = 140 })
-                        local fileinfo = MiniStatusline.section_fileinfo({ trunc_width = 120 })
-                        local location = MiniStatusline.section_location({ trunc_width = math.huge })
-                        -- local search = MiniStatusline.section_searchcount({ trunc_width = 75 })
-
-                        return MiniStatusline.combine_groups({
-                            { hl = mode_hl, strings = { mode } },
-                            { hl = "MiniStatuslineDevinfo", strings = { git, diff, diagnostics, lsp } },
-                            "%<", -- Mark general truncate point
-                            { hl = "MiniStatuslineFilename", strings = { filename } },
-                            "%=", -- End left alignment
-                            { hl = "MiniStatuslineFileinfo", strings = { fileinfo } },
-                            { hl = mode_hl, strings = { location } },
-                        })
-                    end,
-                },
-                use_icons = true,
-            })
-        end,
-    },
-    {
         "mini.icons",
         event = "DeferredUIEnter",
         after = function()
