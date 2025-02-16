6d05b4da0b3725184e8a6250e6863b54ea24b2f0
bartek <bartoszluka1@gmail.com>
2025-02-16 16:57:31 +0100

    Use mini-sessions' default read/write

--- lua/my/sessions.lua
+++ lua/my/sessions.lua
@@ -1,7 +1,7 @@
 local function get_git_branch_name()
     local process = vim.system({ "git", "rev-parse", "--abbrev-ref", "HEAD" }, { text = true }):wait()
     local branch = vim.trim(process.stdout) or "main"
-    return branch
+    return branch:gsub("/", "_")
 end
 
 local function get_session_name()
@@ -16,16 +16,16 @@ end
 
 require("mini.sessions").setup({
     -- Whether to read default session if Neovim opened without file arguments
-    autoread = false,
+    autoread = true,
 
     -- Whether to write currently read session before quitting Neovim
-    autowrite = false, -- I set it up manually
+    autowrite = true,
 
     -- Directory where global sessions are stored (use `''` to disable)
-    directory = vim.fs.joinpath(vim.fn.stdpath("data") --[[@as string]], "session"), --<"session" subdir of user data directory from |stdpath()|>,
+    -- directory = vim.fs.joinpath(vim.fn.stdpath("data") --[[@as string]], "session"), --<"session" subdir of user data directory from |stdpath()|>,
 
     -- File for local session (use `''` to disable)
-    file = "",
+    file = get_session_name(),
 
     -- Whether to force possibly harmful actions (meaning depends on function)
     force = { read = false, write = true, delete = false },
@@ -40,31 +40,4 @@ require("mini.sessions").setup({
 
     -- Whether to print session path after action
     verbose = { read = false, write = false, delete = true },
-})
-
-nx.au({
-    {
-        "VimEnter",
-        callback = function()
-            local session_name = get_session_name()
-            local exists = vim.tbl_contains(vim.tbl_keys(MiniSessions.detected), session_name)
-            if exists then
-                MiniSessions.read(session_name)
-            end
-        end,
-        desc = "auto read session on start",
-    },
-    {
-        "VimLeavePre",
-        callback = function()
-            local session_name = get_session_name()
-            MiniSessions.write(session_name)
-        end,
-        desc = "auto write session on exit",
-    },
-}, {
-
-    once = true,
-    nested = true,
-    create_group = "MySessions",
 })
